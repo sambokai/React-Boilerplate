a52c0776a83e6c4fe096105e1ace217de5bdf8c2
Sam Bokai <sam.bokai@scout24.com>
2018-02-21 21:37:34 +0100

    Add & configure webpack-dev-server

--- webpack.config.js
+++ webpack.config.js
@@ -5,13 +5,18 @@ const path = require('path');
 // Constants with our paths
 const paths = {
     DIST: path.resolve(__dirname, 'dist'),
-    JS: path.resolve(__dirname, 'src/js')
+    JS: path.resolve(__dirname, 'src/js'),
+    SRC: path.resolve(__dirname, 'src'),
 };
 
 module.exports = {
     entry: path.join(paths.JS, 'app.js'),
     output: {
         path: paths.DIST,
-        filename: 'app.bundle.js'
-    }
+        filename: 'app.bundle.js',
+    },
+    //Dev server configuration
+    devServer: {
+        contentBase: paths.SRC,
+    },
 };
