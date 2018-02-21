646a242d3a6c34de8ed1b83382e29d019e4c489e
Sam Bokai <sam.bokai@scout24.com>
2018-02-21 23:17:23 +0100

    Add react and example react component to be rendered

--- src/js/app.js
+++ src/js/app.js
@@ -1,1 +1,14 @@
-console.log("Hello World!");
+import React from 'react';
+import {render} from 'react-dom';
+
+export default class Hello extends React.Component {
+    render() {
+        return (
+            <h3>
+                Hello from React!
+            </h3>
+        )
+    }
+}
+
+render(<Hello/>, document.getElementById('app'));
