867fbd93fc639dc239b00b31f2fd186aaa271a1f
Sam Bokai <sam.bokai@scout24.com>
2018-02-22 01:12:11 +0100

    Add and configure ESLint linting

--- webpack.config.js
+++ webpack.config.js
@@ -28,6 +28,8 @@ module.exports = {
     module: {
         rules: [
             {test: /\.jsx?$/, exclude: /node_modules/, loader: "babel-loader"},
+            {test: /\.jsx?$/, exclude: /node_modules/, loader: "eslint-loader"},
+
         ]
     },
 
