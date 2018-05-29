a078b8cb3ae5b9dcbbc16afa80902cef05fd9c16
Sam Bokai <sam.bokai@scout24.com>
2018-05-29 19:18:15 +0200

    Move index.jsx to src folder

--- src/index.jsx
+++ src/index.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { render } from 'react-dom';
 
-import Hello from './components/Hello';
+import Hello from './js/components/Hello';
 
 render(<Hello />, document.getElementById('app'));
--- webpack.config.js
+++ webpack.config.js
@@ -15,7 +15,7 @@ const paths = {
 
 // Webpack Configuration
 module.exports = {
-  entry: path.join(paths.JS, 'index.jsx'),
+  entry: path.join(paths.SRC, 'index.jsx'),
   output: {
     path: paths.DIST,
     filename: 'app.bundle.js',
