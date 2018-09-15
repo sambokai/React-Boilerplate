8ae73ed71bf5f079bc57239684a487fd3ebeb042
Sam Bokai <sam.bokai@scout24.com>
2018-09-15 15:24:44 +0200

    Split webpack config into dev/prod;

--- webpack.common.js
+++ webpack.common.js
@@ -1,12 +1,7 @@
-// We are using node's native package 'path'
-// https://nodejs.org/api/path.html
 const path = require('path');
 
-// Imports
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
-
-// Constants with our paths
 const paths = {
   DIST: path.resolve(__dirname, 'dist'),
   JS: path.resolve(__dirname, 'src/js'),
@@ -33,13 +28,6 @@ module.exports = {
     ],
   },
 
-  // Enable importing JS files without specifying their's extenstion -> ADDED IN THIS STEP
-  //
-  // So we can write:
-  // import MyComponent from './my-component';
-  //
-  // Instead of:
-  // import MyComponent from './my-component.jsx';
   resolve: {
     extensions: ['.js', '.jsx'],
   },
